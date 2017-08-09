3acc5ec34b702c5a1209330dc400f7658d0fc244
Kristoffer <krissi_sykkel@hotmail.com>
2017-08-09 18:15:57 +0200

    inputfield edit page

--- src/App/Editor/EditorBar/elements.js
+++ src/App/Editor/EditorBar/elements.js
@@ -1,10 +1,12 @@
-import styled from 'styled-components';
-import TextMedium from '../../../components/TextMedium'
+import styled from "styled-components";
+import TextMedium from "../../../components/TextMedium";
+import Button from "../../../components/Button";
 
 export const EditorWrapper = styled.div`
   border-bottom: 2px solid black;
   grid-area: bar;
   display: flex;
+  font-family: sans-serif;
 `;
 
 export const ButtonWrapper = styled.div`
@@ -17,11 +19,28 @@ export const ButtonWrapper = styled.div`
 
 export const TextWrapper = styled.div`
   flex: 1;
-  padding-left: 25px;
   align-items: baseline;
   line-height: 75px;
 `;
 
 export const PageText = styled(TextMedium)`
   text-transform: capitalize;
+`;
+
+export const TitleInput = styled.input`
+  height: 60%;
+  padding: 12px 20px;
+  margin: 8px 35px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+`;
+
+export const StyledButton = styled(Button)`
+
+  height: 60%;
+  padding: 12px 20px;
+  margin: 8px 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px
 `;
--- src/App/Editor/EditorBar/index.js
+++ src/App/Editor/EditorBar/index.js
@@ -1,39 +1,37 @@
-import React from 'react';
-import {connect} from 'cerebral/react';
-import {state, signal} from 'cerebral/tags';
+import React from "react";
+import { connect } from "cerebral/react";
+import { state, signal } from "cerebral/tags";
 import {
   EditorWrapper,
   ButtonWrapper,
   TextWrapper,
-  PageText
-} from './elements';
-import Button from '../../../components/Button';
+  PageText,
+  TitleInput,
+  StyledButton
+} from "./elements";
 
-export default connect({
-  pageName: state`edit.page`,
-  saveClicked: signal`edit.saveClicked`,
-  resetClicked: signal`edit.resetClicked`,
-  cancelClicked: signal`edit.cancelClicked`,
-}, function EditorBar({pageName, saveClicked, resetClicked, cancelClicked}) {
-  return (
-    <EditorWrapper>
-      <TextWrapper>
-        <PageText>{pageName}</PageText>
-      </TextWrapper>
-      <ButtonWrapper>
-        <Button
-          onClick={() => saveClicked()}
-          text={"Lagre"}
-        />
-        <Button
-          onClick={() => resetClicked()}
-          text={"Reset"}
-        />
-        <Button
-          onClick={() => cancelClicked()}
-          text={"Avbryt"}
-        />
-      </ButtonWrapper>
-    </EditorWrapper>
-  )
-});
+export default connect(
+  {
+    pageName: state`edit.page`,
+    saveClicked: signal`edit.saveClicked`,
+    resetClicked: signal`edit.resetClicked`,
+    cancelClicked: signal`edit.cancelClicked`
+  },
+  function EditorBar({ pageName, saveClicked, resetClicked, cancelClicked }) {
+    return (
+      <EditorWrapper>
+        <TextWrapper>
+          <TitleInput placeholder={pageName} />
+          <PageText>
+            {pageName}
+          </PageText>
+        </TextWrapper>
+        <ButtonWrapper>
+          <StyledButton onClick={() => saveClicked()} text={"Lagre"} />
+          <StyledButton onClick={() => resetClicked()} text={"Reset"} />
+          <StyledButton onClick={() => cancelClicked()} text={"Avbryt"} />
+        </ButtonWrapper>
+      </EditorWrapper>
+    );
+  }
+);
