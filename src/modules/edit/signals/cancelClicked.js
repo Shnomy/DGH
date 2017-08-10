4359fc1fd6033ec13b8d9aeb31b1cdf49836d21f
Johannes Andersen <johanes1@online.no>
2017-08-10 14:44:15 +0200

    Added semicolon

--- src/modules/edit/signals/cancelClicked.js
+++ src/modules/edit/signals/cancelClicked.js
@@ -1,6 +1,6 @@
-import {redirect} from '@cerebral/router/operators'
-import {string, state} from 'cerebral/tags';
+import { redirect } from "@cerebral/router/operators";
+import { string, state } from "cerebral/tags";
 
 export default [
-  redirect(string`/${state`edit.page` === 'forside' ? "" : state`edit.page`}`)
-]
+  redirect(string`/${state`edit.page` === "forside" ? "" : state`edit.page`}`)
+];
